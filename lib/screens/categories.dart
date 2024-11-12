1bc0ffbbb9c298f793c084b042bcd91d4f45c01f
Nyctophilus <mohammed.yuossry@gmail.com>
2024-11-12 17:27:11 +0200

    first commit

---
+++ lib/screens/categories.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/widgets/category_item_grid.dart';
+
+class Categories extends StatelessWidget {
+  const Categories({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Pick your category"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: GridView(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20,
+          ),
+          children: [
+            for (final category in availableCategories)
+              CategoryItem(item: category)
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/category_item_grid.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/models/category.dart';
+
+class CategoryItem extends StatelessWidget {
+  const CategoryItem({super.key, required this.item});
+
+  final Category item;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTapCategory,
+      splashColor: Theme.of(context).colorScheme.primary,
+      borderRadius: BorderRadius.circular(12),
+      child: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [item.color.withOpacity(0.5), item.color.withOpacity(0.9)],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Center(
+          child: Text(
+            item.title,
+            style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void onTapCategory() {}
+}
