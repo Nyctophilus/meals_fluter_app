27c45d3195bf956fba688882db47496a574d2c40
Nyctophilus <mohammed.yuossry@gmail.com>
2024-11-20 16:32:59 +0200

    finetuning Explicit animations

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -61,8 +61,14 @@ class _CategoriesState extends State<Categories>
                 })
         ],
       ),
-      builder: (context, child) => Padding(
-        padding: EdgeInsets.only(top: 100 - _animationController.value * 100),
+      builder: (context, child) => SlideTransition(
+        position: Tween(
+          begin: const Offset(0, 0.3),
+          end: const Offset(0, 0),
+        ).animate(CurvedAnimation(
+          parent: _animationController,
+          curve: Curves.easeInOut,
+        )),
         child: child,
       ),
     );
